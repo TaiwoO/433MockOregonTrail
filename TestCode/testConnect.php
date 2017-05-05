34df5368db13d57021f34a68e485e5283bceb310
Stephen <stepmas1@umbc.edu>
2017-05-05 10:55:01 -0400

    Will have to figure something out for DB, may just need to use WAMP server

--- TestCode/testConnect.php
+++ TestCode/testConnect.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "https://studentdb-maria.gl.umbc.edu/";
-$username	= "stepmas1";
+$serverName = "studentdb-maria.gl.umbc.edu";
+$username	= "stepmas1@mysql-admin.umbc.edu";
 $password	= "stepmas1";
 
 //Connect to DB
-$conn = mysqli_connect($servername, $username, $password);
+$conn = mysqli_connect($serverName, $username, $password);
 
 if(mysqli_connect_errno())
 {
